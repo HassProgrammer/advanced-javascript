ef0afd88eaf5c1b1960312fcafba7bb8a3fa328b
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-26 15:20:41 +0600

    double vs triple equal complited

---
+++ double-vstriple-equal.js
@@ -0,0 +1,105 @@
+const first = 2;
+const second = 3;
+
+if(first == second){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+//////////////////////////////////////////////////
+
+const first1 = 2;
+const second1 = 2;
+
+if(first1 == second1){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+//////////////////////////////////////////////////////////////
+
+const first2 = 2;
+const second2 = "2";
+
+if(first2 == second2){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+//////////////////////////////////////////////
+
+const first3 = 2;
+const second3 = "2";
+
+if(first3 === second3){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+/////////////////////////////////////////////
+
+const first4 = 2;
+const second4 = true;
+
+if(first4 == second4){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+/////////////////////////////////////////////
+
+const first5 = 2;
+const second5 = false;
+
+if(first5 == second5){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+///////////////////////////////////////////////
+
+const first6 = 0;
+const second6 = false;
+
+if(first6 == second6){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+///////////////////////////////////////////////
+
+const first7 = 1;
+const second7 = true;
+
+if(first7 == second7){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
+////////////////////////////////////////////
+
+const first8 = 0;
+const second8 = false;
+
+if(first8 === second8){
+    console.log("Condition is true");
+}
+else{
+    console.log("condition is false");
+}
