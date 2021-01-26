eff2e456b493083cec0987ecc54d0392c5e83449
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-26 14:09:48 +0600

    true va false

---
+++ true-false.js
@@ -0,0 +1,23 @@
+const age = 0;
+const age2 = 4;
+const age3 = -4;
+
+if(age3){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
+
+
+
+const name = "Solaiman";
+const name2 = "";
+const name3 = " ";
+
+if(name3){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
