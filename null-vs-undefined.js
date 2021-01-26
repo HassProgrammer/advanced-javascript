c0d07d3fe76290095f7e789a9f714b264c28a009
HassProgrammer <md.ahshanulalam@gmail.com>
2021-01-26 14:38:26 +0600

    null vs undefined cleard

---
+++ null-vs-undefined.js
@@ -0,0 +1,34 @@
+let pakhi;
+console.log(pakhi);
+
+///////////////////////////////
+
+function add(num1, num2){
+    console.log(num1 + num2);
+
+}
+const result = add(13, 12);
+console.log(result);
+
+//////////////////////////////
+
+function add2(num1, num2){
+    console.log(num1, num2);
+}
+const result2 = add2(13);
+console.log(result2);
+
+///////////////////////////////
+
+const premik = {name:"smart dude", phone: 453827};
+console.log(premik.gf);
+
+////////////////////////////////////////////////////////
+
+let fun= undefined;
+console.log(fun);
+
+//////////////////////
+
+let ages = [2, 5, 9]
+console.log(ages[11]);
